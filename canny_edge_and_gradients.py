3ec0f272683e96d948e2cb08c5fc8e087eab3eca
smnnsmol <nikolai.smolnikow@tu-dortmund.de>
2017-09-13 20:18:53 +0200

    Add Canny Edge Detection and Gradient Methods

---
+++ canny_edge_and_gradients.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('images/cat.jpg')
+img = cv2.imread('images/god.jpg')
+
+laplacian = cv2.Laplacian(img, cv2.CV_64F)
+sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=19)
+sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=19)
+
+edges = cv2.Canny(img, 150, 250)
+
+cv2.imshow('Original', img)
+cv2.imshow('Laplacian', laplacian)
+cv2.imshow('SobelX', sobelx)
+cv2.imshow('SobelY', sobely)
+cv2.imshow('Edges', edges)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
