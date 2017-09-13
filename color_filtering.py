8822136efbd20b9aeb3aea52e5a2987a91127051
smnnsmol <nikolai.smolnikow@tu-dortmund.de>
2017-09-13 19:39:57 +0200

    ADD various filters

--- color_filtering.py
+++ color_filtering.py
@@ -2,25 +2,34 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread('images/background_lock.jpg', cv2.IMREAD_COLOR)
+img = cv2.imread('images/fanta_logo.jpg', cv2.IMREAD_COLOR)
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+img_resized = cv2.resize(img,None,fx=0.25, fy=0.25, interpolation = cv2.INTER_CUBIC)
+cv2.imshow('Original',img_resized)
+
+hsv = cv2.cvtColor(img_resized, cv2.COLOR_BGR2HSV)
 
 # BGR
-lower_blue = np.array([50, 0, 0])
+lower_blue = np.array([105, 0, 0])
 upper_blue = np.array([255, 255, 255])
-
-mask_blue = cv2.inRange(hsv, lower_blue,upper_blue)
-res_blue = cv2.bitwise_and(img, img, mask = mask_blue)
+mask = cv2.inRange(hsv, lower_blue,upper_blue)
+res = cv2.bitwise_and(img_resized, img_resized, mask = mask)
+cv2.imshow('Mask',res)
 
 kernel = np.ones((15,15), np.float32) / 225
-smoothed = cv2.filter2D(res_blue, -1, kernel)
+smoothed = cv2.filter2D(res, -1, kernel)
+cv2.imshow('Averaging',smoothed)
 
-# cv2.imshow('mask', mask_blue)
-# cv2.imshow('blue_filter', res_blue)
-cv2.imshow('mask', mask_blue)
-cv2.imshow('red_filter', res_blue)
-cv2.imshow('smooth', res_blue)
+blur = cv2.GaussianBlur(res,(15,15),0)
+cv2.imshow('Gaussian Blurring',blur)
+
+# Seems to have the best Performance
+median_blur = cv2.medianBlur(res,15)
+cv2.imshow('Median Blurring',median_blur)
+
+bilateral_blur = cv2.bilateralFilter(res, 15, 75, 75)
+cv2.imshow('Bilateral Blurring',bilateral_blur)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
