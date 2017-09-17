bc09b867eaa4c47caa64c414a262ab8433f89f95
smnnsmol <nikolai.smolnikow@tu-dortmund.de>
2017-09-17 14:05:55 +0200

    Add Haarascade

--- mog_background_reduction.py
+++ mog_background_reduction.py
@@ -1,21 +1,22 @@
 import numpy as np
 import cv2
 
-#cap = cv2.VideoCapture('videos/people-walking.mp4')
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('videos/people-walking.mp4')
+#cap = cv2.VideoCapture(0)
 fgbg = cv2.createBackgroundSubtractorMOG2()
-fgbg_knn = cv2.createBackgroundSubtractorKNN()
+## Alternative
+#fgbg_knn = cv2.createBackgroundSubtractorKNN()
 
 while(1):
     ret, frame = cap.read()
 
     fgmask = fgbg.apply(frame)
-    fgmask_knn = fgbg_knn.apply(frame)
+    #fgmask_knn = fgbg_knn.apply(frame)
     median_blur = cv2.medianBlur(fgmask,7)
 
     cv2.imshow('frame',frame)
     cv2.imshow('fgmask',fgmask)
-    cv2.imshow('fgmask_knn',fgmask_knn)
+    #cv2.imshow('fgmask_knn',fgmask_knn)
     cv2.imshow('median_blur',median_blur)
 
 
