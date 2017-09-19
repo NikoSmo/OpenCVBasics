6f95ab80f1d6626d7c4dada8194c5ff9c1653402
smnnsmol <nikolai.smolnikow@tu-dortmund.de>
2017-09-19 23:31:56 +0200

    Add haarcascade_mug classifier, not working :( , trying watch classifier

--- haarcascade_mug/README.md
+++ haarcascade_mug/README.md
@@ -1,1 +1,5 @@
 This Project attempts to train a haarcascades detecting Mugs
+
+Important :
+When creating samples on Linux via opencv_createsamples make sure that the bg.txt doesnt have windows CRLF line terminators
+-> convert with dos2unix
---
+++ haarcascade_mug/haarcascade_mug.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+#this is the cascade we just made. Call what you want
+mug_cascade = cv2.CascadeClassifier('cascade_mug.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # image, reject levels level weights.
+    mugs = mug_cascade.detectMultiScale(gray, 50, 50)
+
+    # add this
+    for (x,y,w,h) in mugs:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+    cv2.imshow('img',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
